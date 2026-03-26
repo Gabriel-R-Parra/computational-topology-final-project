699a676b9ca797a8b02cc1d5d9c61422856f5ac9
u0848511 <bhbte@DESKTOP-KVOHPD1.localdomain>
2026-03-25 18:19:31 -0600

    Used Takens' embeddings

---
+++ jupyter/FinalPersistenceBarcodeGenerator.py
@@ -0,0 +1,59 @@
+# Hello again!
+
+import subprocess
+import os
+
+def makeBarcodes(pointCloud: str):
+    result = subprocess.run(
+        ["./ripser", "--format", "point-cloud", f"../pointCloudsReal/{pointCloud}"],
+        capture_output=True,
+        text=True
+    )
+
+    resultStart = (result.stdout).find("persistence intervals in dim 0:")
+    resultEnd = (result.stdout).find("persistence intervals in dim 1:")
+
+    resultDimZero = (result.stdout)[resultStart: resultEnd]
+    resultDimOne = (result.stdout)[resultEnd:]
+
+    print("A")
+
+    # print(result.stdout)
+    # print(resultDimZero)
+    # print(resultDimOne)
+
+    pointCloudNoEnding = pointCloud[: -6]
+
+    # "../persistenceBarcodes/{pointCloud[8:10]}/Dim0/{pointCloud[: len(pointCloud) - 14]}PBD0.txt"
+    """
+    with open(f"examples/pointclouds/Barcodes/Dim0/{pointCloudNoEnding}PBD0.txt", "w") as f:
+        f.write(resultDimZero)
+        # print("testing")
+
+    with open(f"examples/pointclouds/Barcodes/Dim1/{pointCloudNoEnding}PBD1.txt", "w") as f:
+        f.write(resultDimOne)
+        # print("testing")
+    """
+    
+    with open(f"../persistenceBarcodes/{pointCloud[8:10]}/Dim0/{pointCloud[: len(pointCloud) - 14]}PBD0.txt", "w") as f:
+        f.write(resultDimZero)
+
+    print("B")
+
+    with open(f"../persistenceBarcodes/{pointCloud[8:10]}/Dim1/{pointCloud[: len(pointCloud) - 14]}PBD1.txt", "w") as f:
+        f.write(resultDimOne)
+
+def main():
+    print("Hello, World!")
+    
+    pointClouds = os.listdir("../pointCloudsReal")
+
+    for pointCloud in pointClouds:
+        print(pointCloud)
+        # print(pointCloud[: len(pointCloud) - 14])
+        makeBarcodes(pointCloud)
+
+    # makeBarcodes("bottle-01PC.txt")
+
+if __name__ == "__main__":
+    main()
